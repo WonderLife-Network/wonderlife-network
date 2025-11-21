798c041bcd1f54426e8e24f08b4f107cf97efb7c
WonderLife-Network <info@wonderlife.city>
2025-11-22 00:41:23 +0100

    Update route.ts

--- app/api/forum/thread/delete/route.ts
+++ app/api/forum/thread/delete/route.ts
@@ -2,7 +2,11 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 export async function POST(req: Request) {
-  const { id } = await req.json();
+  const { id, role } = await req.json();
+
+  if (role !== "ADMIN" && role !== "OWNER") {
+    return NextResponse.json({ error: "Keine Berechtigung" });
+  }
 
   await prisma.forumReply.deleteMany({
     where: { threadId: Number(id) }
