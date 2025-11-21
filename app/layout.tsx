cf7858c8f71222ce7bbe175f2ba41d1ebfb2b019
WonderLife-Network <info@wonderlife.city>
2025-11-21 20:30:41 +0100

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import "./globals.css";
+
+export const metadata = {
+    title: "WonderLife Network",
+    description: "Offizielle Webseite des WonderLife City Netzwerks"
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="de">
+            <body className="bg-[#08090F] text-white antialiased">
+                {children}
+            </body>
+        </html>
+    );
+}
